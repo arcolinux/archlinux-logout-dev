48caead8754334c3b6c3588fe7bc47ea055ee6a8
ArcoLinux <arcolinuxinfo@gmail.com>
2021-10-25 15:58:41 +0200

    pkill

--- usr/share/arcologout/Functions.py
+++ usr/share/arcologout/Functions.py
@@ -118,8 +118,62 @@ def _get_logout():
     desktop = out.stdout.decode().split("=")[1].strip()
 
     print("Your desktop is " + desktop)
-    if desktop :
-        return "pkill -KILL -u $USER"
+    if desktop in ("herbstluftwm", "/usr/share/xsessions/herbstluftwm"):
+        return "herbstclient quit"
+    elif desktop in ("bspwm", "/usr/share/xsessions/bspwm"):
+        return "pkill bspwm"
+    elif desktop in ("jwm", "/usr/share/xsessions/jwm"):
+        return "pkill jwm"
+    elif desktop in ("openbox", "/usr/share/xsessions/openbox"):
+        return "pkill openbox"
+    elif desktop in ("awesome", "/usr/share/xsessions/awesome"):
+        return "pkill awesome"
+    elif desktop in ("qtile", "/usr/share/xsessions/qtile"):
+        return "pkill qtile"
+    elif desktop in ("xmonad", "/usr/share/xsessions/xmonad"):
+        return "pkill xmonad"
+    #for lxdm
+    elif desktop in ("Xmonad", "/usr/share/xsessions/xmonad"):
+        return "pkill xmonad"
+    elif desktop in ("dwm", "/usr/share/xsessions/dwm"):
+        return "pkill dwm"
+    elif desktop in ("i3", "/usr/share/xsessions/i3"):
+        return "pkill i3"
+    elif desktop in ("i3-with-shmlog", "/usr/share/xsessions/i3-with-shmlog"):
+        return "pkill i3-with-shmlog"
+    elif desktop in ("lxqt", "/usr/share/xsessions/lxqt"):
+        return "pkill lxqt"
+    elif desktop in ("spectrwm", "/usr/share/xsessions/spectrwm"):
+        return "pkill spectrwm"
+    elif desktop in ("xfce", "/usr/share/xsessions/xfce"):
+        return "xfce4-session-logout -f -l"
+    elif desktop in ("sway", "/usr/share/xsessions/sway"):
+        return "pkill sway"
+    elif desktop in ("icewm", "/usr/share/xsessions/icewm"):
+        return "pkill icewm"
+    elif desktop in ("icewm-session", "/usr/share/xsessions/icewm-session"):
+        return "pkill icewm-session"
+    elif desktop in ("cwm", "/usr/share/xsessions/cwm"):
+        return "pkill cwm"
+    elif desktop in ("fvwm3", "/usr/share/xsessions/fvwm3"):
+        return "pkill fvwm3"
+    elif desktop in ("stumpwm", "/usr/share/xsessions/stumpwm"):
+        return "pkill stumpwm"
+    elif desktop in ("leftwm", "/usr/share/xsessions/leftwm"):
+        return "pkill leftwm"
+    elif desktop in ("dusk", "/usr/share/xsessions/dusk"):
+        return "pkill dusk"
+    elif desktop in ("wmderland", "/usr/share/xsessions/wmderland"):
+        return "pkill wmderland"
+    elif desktop in ("gnome", "/usr/share/xsessions/gnome"):
+        return "gnome-session-quit --logout --no-prompt"
+    elif desktop in ("gnome-xorg", "/usr/share/xsessions/gnome-xorg"):
+        return "gnome-session-quit --logout --no-prompt"
+    elif desktop in ("gnome-classic", "/usr/share/xsessions/gnome-classic"):
+        return "gnome-session-quit --logout --no-prompt"
+    elfi desktop:
+		return "pkill -KILL -u $USER"
+    return None
 
 
 def button_active(self, data, GdkPixbuf):
